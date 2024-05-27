d802013916e8b15a9f86143905cf139e8f78627e
kamleshhargun <kamleshhargun@gmail.com>
2024-05-27 07:07:19 +0000

    ok

--- 01_variables.js
+++ 01_variables.js
@@ -2,3 +2,12 @@ const accountId = 225507
 let accountEmail = " naresh@gmail.com "
 var accountPassword = " 12345 "
 accountCity = " Jaipur "
+accountState = " rajasthan "
+
+accountEmail = " c@hc.com "
+accountPassword = " 21212121 "
+accountCity = " Bangluru "
+accountState = " gujarat "
+console.log([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
