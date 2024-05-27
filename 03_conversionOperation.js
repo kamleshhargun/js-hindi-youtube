1e86d2a110b16049b148b2a4542c2df4e697e258
kamleshhargun <kamleshhargun@gmail.com>
2024-05-27 07:18:37 +0000

    3

---
+++ 03_conversionOperation.js
@@ -0,0 +1,42 @@
+
+let isLoggedIn = "kamlesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let scor = 33
+
+const {scire} = requestAnimationFrame.body
+console.log(types of score);
+console.log(types of (score));
+
+
+let valueInNumber = Number(score)
+console.log(types of score);
+
+ "33" => 33
+ "33abc" => NaN
+ true => 1: false => 0
+
+
+ 1 => true; 0 => false
+ "" => false
+ "kamlesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+let value = 3
+let negValue = -value
+ console.log(negValue);
+
+console.log(2+2);
+
+let str1 = " naksh "
+let str2 = " hargun "
+
+let str3 = str1 + str2
+console.log(str3);
