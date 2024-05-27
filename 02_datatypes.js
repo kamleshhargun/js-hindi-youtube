1be71fc9f25ea96d0d7321a2028f865b8f42abb3
kamleshhargun <kamleshhargun@gmail.com>
2024-05-27 07:11:02 +0000

    2

---
+++ 02_datatypes.js
@@ -0,0 +1,9 @@
+  console.log(" hitesh" )
+  let name = "kamlesh"
+    let age = 38
+  let isLoggedIn = false
+let state = null
+  console.log(typeof undefined ); // undefinded
+
+  console.log(typeof null); // object
+
