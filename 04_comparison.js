e09b88c6b7cd4267c72287d973011577fbf5cf1f
kamleshhargun <kamleshhargun@gmail.com>
2024-05-27 10:05:07 +0000

    4

--- 04_comparison.js
+++ 04_comparison.js
@@ -8,6 +8,14 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// console.log(1 == 1); true
+// console.log("2" == 2); true
+// console.log("2" == 2); false
