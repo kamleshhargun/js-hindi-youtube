346a02a2192758360c5d56f73c64cc5325099884
kamleshhargun <kamleshhargun@gmail.com>
2024-05-27 06:09:17 +0000

    01

---
+++ 01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 225507
+let accountEmail = " naresh@gmail.com "
+var accountPassword = " 12345 "
+accountCity = " Jaipur "
+
+console.table[ (accountId, accountEmail, accountPassword, accountCity )];
