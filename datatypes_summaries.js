82959f5195e1f05b4ece64b6bc02ba326246ad73
kamleshhargun <kamleshhargun@gmail.com>
2024-05-27 10:29:48 +0000

    datatypes-summary

---
+++ datatypes_summaries.js
@@ -0,0 +1,34 @@
+// Primitive
+
+// 7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+// const score= 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let unserEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 87654398767768n
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "kamlesh",
+    age: 38,
+}
+
+console.log(myObj);
+const myFunction = function(){}
+console.log("Hellow World");
+
+
+console.log(typeof anotherId);
