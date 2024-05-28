6f04db87197ea06e9ef9a569cf7b134babf39233
kamleshhargun <kamleshhargun@gmail.com>
2024-05-28 13:21:35 +0000

    03_objects

--- 03_objects.js
+++ 03_objects.js
@@ -3,14 +3,31 @@
 
 // Object.create
 
-const JsUser = {
-    name: "kamlesh",
-    age: 38,
-    location: "jaipur"
-    email: "kamlesh@gmail.com"
-    isLoggedIn: false,
-    lastLoginDays: ["Monday", "Saturday"]
+// const mySym = Symbol("key1")
+
+// const JsUser = {
+//     name: "Kamlesh",
+//     "full name": "Kamlesh Hargun",
+//     [mySym]: "myKey1",
+//     age: 38,
+//     location: "jaipur"
+//     email: "kamlesh@gmail.com",
+//     isLoggedIn: false,
+//     lastLoginDays: ["Monday", "Saturday"]
+// }
+
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email = "hitesh@chatgpt.com"
+Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = fuction(){
+    console.log("Hellow JS user")
 }
 
-console.log(JsUser.email);
-console.log(JsUser["email"]);
+console.log(JsUser.greeting);
