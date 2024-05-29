fa5895fd489a0eb7c54d47e22948a54d5ead7a59
kamleshhargun <kamleshhargun@gmail.com>
2024-05-29 11:53:59 +0000

    iife

---
+++ 04_iife.js
@@ -0,0 +1,16 @@
+// Immediately Invoked Function Expressions ( IIFE)
+
+(function chai(){
+    console.log('DB CONNECTED');
+})();
+
+function chai(){
+    console.log('DB CONNECTED');
+}
+
+chai();
+
+( (name) => {
+    console.log(('DB CONNECTED TWO ${name}'));
+})()
+
